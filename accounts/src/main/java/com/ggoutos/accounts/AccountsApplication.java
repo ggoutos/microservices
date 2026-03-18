5b84f32fb7d7e685d7900e863815ae3b6b12f444
George Goutos <ggoutos@outlook.com>
2026-03-18 13:34:10 +0200

    Initial commit - accounts microservice

---
+++ accounts/src/main/java/com/ggoutos/accounts/AccountsApplication.java
@@ -0,0 +1,13 @@
+package com.ggoutos.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}
