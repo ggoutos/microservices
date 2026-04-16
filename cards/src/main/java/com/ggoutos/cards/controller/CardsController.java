809382b0f5a7b1cf32480c08ac5f4b392485e594
George Goutos <ggoutos@outlook.com>
2026-04-16 15:09:36 +0300

    Add OpenTelemetry instrumentation with Tempo and Grafana support. Configure trace context propagation, update logging patterns, and introduce dependencies for Java agent integration.

--- cards/src/main/java/com/ggoutos/cards/controller/CardsController.java
+++ cards/src/main/java/com/ggoutos/cards/controller/CardsController.java
@@ -45,8 +45,9 @@ public class CardsController {
     public ResponseEntity<CardsDto> fetchCardDetails(
             @RequestHeader(name = "eazybank-correlation-id", required = true) String correlationId,
             @RequestParam @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number must be 10 digits") String mobileNumber) {
-        log.info("Correlation ID: {}", correlationId);
+        log.info("fetchCardDetails call start");
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        log.info("fetchCardDetails call end");
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
